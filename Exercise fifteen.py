1afaa307e444a80023faefd09d0ff1f6bb70fe87
wajj13 <2784154491@qq.com>
2023-03-27 17:20:53 +0800

    PRACTICE CUTOFF 20230325

--- Exercise fifteen.py
+++
@@ -1,9 +0,0 @@
-# 获取文件大小与各个文件大小的和
-import os
-
-print(os.path.getsize("G:/IDEA/PYTHON PROJECT/practionofpython/practicefile/practice14.txt"))
-sum_size = 0
-for file in os.listdir("G:/IDEA/PYTHON PROJECT/practionofpython/practicefile"):
-    if os.path.isfile(file):
-            sum_size += os.path.getsize(file)
-print(sum_size / 1000)
--- Exercise ten.py
+++
@@ -1,6 +0,0 @@
-#原地排序和全排序
-L1 = [10,20,30,4,5,40]
-L1.sort()
-print(L1)
-L2 = sorted(L1, reverse=True)
-print(L2)
